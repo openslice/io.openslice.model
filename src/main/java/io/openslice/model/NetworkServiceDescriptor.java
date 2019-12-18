7c2645e6dd9af6d109b070787a0e5aa1cdc50573
Christos Tranoris <tranoris@gmail.com>
2019-12-19 00:09:59 +0200

    added NetworkServiceDescriptor

---
+++ src/main/java/io/openslice/model/NetworkServiceDescriptor.java
@@ -0,0 +1,12 @@
+package io.openslice.model;
+
+import javax.persistence.Entity;
+
+/**
+ * @author ctranoris
+ *
+ */
+@Entity(name = "NetworkServiceDescriptor")
+public class NetworkServiceDescriptor extends ExperimentMetadata {
+
+}
